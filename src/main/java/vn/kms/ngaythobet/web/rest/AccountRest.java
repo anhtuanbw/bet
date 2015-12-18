caa2260d65a97ca386003ed8b9590e0fe4d81456
HieuVu <hieuvu@kms-technology.com>
2015-12-18 17:51:48 +0700

    format code

--- src/main/java/vn/kms/ngaythobet/web/rest/AccountRest.java
+++ src/main/java/vn/kms/ngaythobet/web/rest/AccountRest.java
@@ -107,7 +107,7 @@ public class AccountRest {
     @RequestMapping(value = "/account/change-password", method = POST)
     public Token changePassword(@Valid @RequestBody ChangePasswordInfo passwordInfo) {
         userService.changePassword(passwordInfo);
-        return login(SecurityUtil.getCurrentLogin(),passwordInfo.getPassword());
+        return login(SecurityUtil.getCurrentLogin(), passwordInfo.getPassword());
     }
 
     @RequestMapping(value = "/logout", method = POST)
