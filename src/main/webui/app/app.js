04dbe7b365918bd02863f5e9b2adc2ad546b6d5f
Viet Huy Vo <vietvo@kms-technology.com>
2015-12-21 15:55:35 +0700

    add component activate

--- src/main/webui/app/app.js
+++ src/main/webui/app/app.js
@@ -6,13 +6,15 @@ import {common} from './common/common'; // jshint ignore:line
 import HomeController from './components/home/home';
 import ResetPasswordController from './common/reset-password/reset-password';
 import ChangePasswordController from './common/change-password/change-password';
+import ActivateController from './components/activate/activate';
 
 export default class AppController {
   /* @ngInject */
   constructor($router) {
     $router.config([
       { path: '/home', component: 'home' },
-      { path: '/', redirectTo: '/home' }
+      { path: '/', redirectTo: '/home' },
+      { path: '/activate', component: 'activate' }
     ]);
   }
 }
@@ -31,6 +33,7 @@ angular.module('ngaythobet', [
 .controller('HomeController', HomeController)
 .controller('ResetPasswordController', ResetPasswordController)
 .controller('ChangePasswordController', ChangePasswordController)
+.controller('ActivateController', ActivateController)
 .config(/* @ngInject */($compileProvider, $componentLoaderProvider, $translateProvider) => {
   // disables AngularJS debug info
   $compileProvider.debugInfoEnabled(false);
