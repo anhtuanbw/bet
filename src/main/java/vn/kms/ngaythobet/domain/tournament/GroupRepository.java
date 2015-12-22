d9425776b0371db20f951387c005a1789912c2da
thangpham <thangpham@kms-technology.com>
2015-12-22 14:46:18 +0700

    Format GroupRepository.java

--- src/main/java/vn/kms/ngaythobet/domain/tournament/GroupRepository.java
+++ src/main/java/vn/kms/ngaythobet/domain/tournament/GroupRepository.java
@@ -2,6 +2,6 @@ package vn.kms.ngaythobet.domain.tournament;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GroupRepository extends JpaRepository<Group, Long>{
-    
+public interface GroupRepository extends JpaRepository<Group, Long> {
+
 }
