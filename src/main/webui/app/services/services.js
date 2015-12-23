518d8a2363446a5aa9a1ca616ad926df0da6c589
Viet Huy Vo <vietvo@kms-technology.com>
2015-12-23 15:00:28 +0700

    modify bower.json, remove register.service.js

--- src/main/webui/app/services/services.js
+++ src/main/webui/app/services/services.js
@@ -6,14 +6,11 @@ import AccountService from './account.service';
 import TournamentService from './tournament.service';
 import RoundService from './round.service';
 import GroupService from './group.service';
-import TournamentService from './tournament.service';
 
 export const services = angular
     .module('ngaythobet.services', [])
     .service('CacheService', CacheService)
     .service('AccountService', AccountService)
-    .service('RegisterService', RegisterService)
     .service('TournamentService', TournamentService)
-    .service('RegisterService', RegisterService)
     .service('RoundService', RoundService)
     .service('GroupService', GroupService);
