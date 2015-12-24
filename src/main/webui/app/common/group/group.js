6c4f5001a662b42fe3d9865a9f8acf9f5a831d80
longntran <longntran@kms-technology.com>
2015-12-24 08:47:00 +0700

    - Use ngMaterial to create dialog create group - Create autocomplete when select moderator - Update group view

--- src/main/webui/app/common/group/group.js
+++ src/main/webui/app/common/group/group.js
@@ -2,90 +2,49 @@
 
 export default class GroupController {
   /* @ngInject */
-  constructor(GroupService, TournamentService, $modalInstance, toaster) {
+  constructor(GroupService, TournamentService, $mdDialog) {
     this.groupService = GroupService;
     this.tournamentService = TournamentService;
-    this.modalInstance = $modalInstance;
-    this.toaster = toaster;
+    this.mdDialog = $mdDialog;
 
-    // This function just add to test, will remove in next commit
-    this.states = this.tournamentService.findAll()
-      .then(response => {
-        if (response.status === 200) {
-          var tournaments = response.data;
-          console.log(tournaments);
-         return tournaments.map( function (repo) {
-          repo.value = repo.name.toLowerCase();
-          return repo;
-        });
-        } else {
-        }
-      });
-
+    this.states = this.getTournament();
+    this.selectedItem = null;
+    this.searchText = null;
     this.querySearch = this.querySearch;
-    this.selectedItemChange = this.selectedItemChange;
     this.groupData = {};
     this.error = {};
   }
 
-  create() {
-    var self = this;
-    self.popTitle = 'Title';
-
-    self.pop = function(type, title, content) {
-      this.toaster.pop(type, title, content);
-    };
-
-    this.groupService.create(self.groupData)
-    .then(response => {
-      if (response.status === 200) {
-        self.closeGroup();
-        self.pop('success', self.popTitle, 'Create success');
-      } else {
-        self.pop('warning', self.popTitle, response.data.message);
-      }
-    })
-    .catch(response => {
-      self.pop('error', self.popTitle, response.data.message);
-    });
+  getTournament() {
+   return this.tournamentService.findAll()
+      .then(response => {
+        if (response.status === 200) {
+          var tournaments = response.data;
+          return tournaments.map( function (repo) {
+            return repo;
+          });
+        }
+      });
   }
 
   querySearch(query) {
-    var results = query ? this.states.filter( this.createFilterFor(query) ) : this.states;
-    return results;
-  }
-
-  selectedItemChange(item) {
-    if(item) {
-      this.groupData.moderator = item.id;
-    }
-  }
-
-  getTournament() {
-    this.tournamentService.findAll()
-    .then(response => {
-      if (response.status === 200) {
-        var tournaments = response.data;
-        console.log(tournaments);
-       return tournaments.map( function (repo) {
-        repo.value = repo.name.toLowerCase();
-        return repo;
-      });
-      } else {
-      }
-    });
+    var self = this;
+    return query ? self.states.filter(self.createFilterFor(query) ) : self.states;
   }
 
   createFilterFor(query) {
-    
     var lowercaseQuery = angular.lowercase(query);
     return function filterFn(state) {
-      return (state.name.indexOf(lowercaseQuery) === 0);
+      return (state.value.indexOf(lowercaseQuery) === 0);
     };
   }
 
-  closeGroup() {
-    this.modalInstance.dismiss();
+  save($event) {
+    // Create group
+  }
+
+  cancel($event) {
+    this.mdDialog.cancel();
   }
 
 }
