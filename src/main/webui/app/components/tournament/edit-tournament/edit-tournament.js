9c4f82e02f05d965ab7cf153a0a76aee5a79a6fb
Viet Huy Vo <vietvo@kms-technology.com>
2015-12-24 09:31:03 +0700

    active tournament

--- src/main/webui/app/components/tournament/edit-tournament/edit-tournament.js
+++ src/main/webui/app/components/tournament/edit-tournament/edit-tournament.js
@@ -2,9 +2,23 @@
 
 export default class EditTournamentController {
   constructor(TournamentService, $rootScope) {
+    this.tournamentService = TournamentService;
     this.tournamentInfo = {};
 	  $rootScope.$on('selectTournament', (event, tournamentInfo) => {
       this.tournamentInfo = tournamentInfo;
+      console.log(this.tournamentInfo);
+    });
+  }
+  
+  activeTournament() {
+    this.tournamentService.active(this.tournamentInfo.id)
+    .then(() => {
+      this.tournamentInfo.activated = true;
+      console.log('success');
+    })
+    .catch(error => {
+      console.log(this.tournamentInfo.id);
+      console.log(error);
     });
   }
 }
