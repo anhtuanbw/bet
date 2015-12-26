a4a1ee7c9271675004c7a8e76dfe76f402305346
tamvvo <tamvvo@kms-technology.com>
2015-12-26 11:19:18 +0700

    Completed create-match

--- src/main/webui/app/common/match/create-match/date-time-picker.js
+++ src/main/webui/app/common/match/create-match/date-time-picker.js
@@ -3,13 +3,30 @@
 
 export default function () {
   return {
+    require: '?ngModel',
     restrict: 'A',
-    link: function (scope, element, attrs, ngModelCtrl) {
-      $(function () {
+    link: function (scope, element, attrs, ngModel) {
+
+      if (!ngModel) return; // do nothing if no ng-model
+
+      ngModel.$render = function () {
+        element.find('input').val(ngModel.$viewValue || '');
+      }
+
       element.datetimepicker({
-        format: 'YYYY-MM-DD hh:mm A'
-        });
+        format: 'YYYY-MM-DD HH:mm'
       });
+
+      element.on('dp.change', function () {
+        scope.$apply(read);
+      });
+
+      read();
+
+      function read() {
+        var value = element.find('input').val();
+        ngModel.$setViewValue(value);
+      }
     }
   }
 }
