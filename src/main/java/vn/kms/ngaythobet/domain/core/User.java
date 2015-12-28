12341c88d939639f0b993daa4e92facb72bc47f5
hoanghnguyen <hoanghnguyen@kms-technology.com>
2015-12-28 14:27:40 +0700

    fix for build

--- src/main/java/vn/kms/ngaythobet/domain/core/User.java
+++ src/main/java/vn/kms/ngaythobet/domain/core/User.java
@@ -27,10 +27,10 @@ public class User extends AuditableEntity {
             return "ROLE_" + name();
         }
     }
-
+    
+    @AuditIgnore
     @ManyToMany(mappedBy = "members")
     @JsonIgnore
-    @MongoDbRef
     private List<Group> groups;
 
     @Column
