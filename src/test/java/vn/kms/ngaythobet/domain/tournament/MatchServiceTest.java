eeadfba5fa292dbb03fa122cc74e33fa279e457f
tungvo <tungvo@kms-technology.com>
2015-12-30 16:08:29 +0700

    + refine unit test for Match API

--- src/test/java/vn/kms/ngaythobet/domain/tournament/MatchServiceTest.java
+++ src/test/java/vn/kms/ngaythobet/domain/tournament/MatchServiceTest.java
@@ -20,7 +20,9 @@ import vn.kms.ngaythobet.web.dto.CreateMatchInfo;
 import vn.kms.ngaythobet.web.dto.UpdateScoreInfo;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.notNullValue;;
+import static org.hamcrest.Matchers.notNullValue;
+
+;
 
 public class MatchServiceTest extends BaseTest {
     @Rule
@@ -73,7 +75,7 @@ public class MatchServiceTest extends BaseTest {
         tournament.setNumOfCompetitors(11L);
         tournament.setName("Euro");
         tournamentTemp = tournamentRepo.save(tournament);
-        
+
         // Create a round belongs to above tournament
         Round round = new Round();
         round.setName("round 1");
@@ -83,49 +85,32 @@ public class MatchServiceTest extends BaseTest {
         List<Round> rounds = new ArrayList<Round>();
         rounds.add(roundTemp);
         // Create competitors
-        Competitor competitor1 = new Competitor();
-        competitor1.setName("England");
-        competitor1.setTournament(tournamentTemp);
-        competitor1.setRounds(rounds);
-        competitorTemp1 = competitorRepo.save(competitor1);
-        Competitor competitor2 = new Competitor();
-        competitor2.setName("France");
-        competitor2.setTournament(tournamentTemp);
-        competitor2.setRounds(rounds);
-        competitorTemp2 = competitorRepo.save(competitor2);
-        
-        Competitor competitor3 = new Competitor();
-        competitor3.setName("England");
-        competitor3.setTournament(tournamentTemp);
-        competitor3.setRounds(rounds);
-        competitorTemp3 = competitorRepo.save(competitor3);
-        Competitor competitor4 = new Competitor();
-        competitor4.setName("France");
-        competitor4.setTournament(tournamentTemp);
-        competitor4.setRounds(rounds);
-        competitorTemp4 = competitorRepo.save(competitor4);
-        
+        competitorTemp1 = createCompetitor("Brazil", tournamentTemp, rounds);
+        competitorTemp2 = createCompetitor("France", tournamentTemp, rounds);
+        competitorTemp3 = createCompetitor("England", tournamentTemp, rounds);
+        competitorTemp4 = createCompetitor("Germany", tournamentTemp, rounds);
+
         competitors = competitorRepo.findAll();
-        
+
         // Add competitors into round
         round.setCompetitors(competitors);
         roundTemp = roundRepo.save(round);
-        
+
         User defaultUser = getDefaultUser();
         String username = defaultUser.getUsername();
         mockLoginUser(username);
 
         User user = userService.getUserInfo();
-        
-        //Create a group join above tournament
+
+        // Create a group join above tournament
         Group group = new Group();
         group.setName("group 1");
         group.setTournament(tournamentTemp);
         group.setModerator(user);
 
         groupRepo.save(group);
-        
-        //Create a match
+
+        // Create a match
         Match match = new Match();
         match.setComment("abc");
         match.setCompetitor1(competitorTemp3);
@@ -136,6 +121,15 @@ public class MatchServiceTest extends BaseTest {
         match.setScore1(1L);
         match.setScore2(2L);
         matchTemp = matchRepo.save(match);
+    }
+
+    private Competitor createCompetitor(String name, Tournament tournament,
+            List<Round> rounds) {
+        Competitor competitor = new Competitor();
+        competitor.setName(name);
+        competitor.setTournament(tournament);
+        competitor.setRounds(rounds);
+        return competitorRepo.save(competitor);
     }
 
     @Test
@@ -151,23 +145,23 @@ public class MatchServiceTest extends BaseTest {
         matchService.createMatch(createMatchInfo);
         assertThat(matchRepo.findAll().size(), equalTo(2));
     }
-    
+
     @Test
     public void testGetMatchById() {
-        assertThat(matchService.getMatch(matchTemp.getId()),notNullValue());
+        assertThat(matchService.getMatch(matchTemp.getId()), notNullValue());
     }
-    
+
     @Test
     public void testUpdateScore() {
         long score1 = 22;
         long score2 = 22;
-        
+
         UpdateScoreInfo updateScoreInfo = new UpdateScoreInfo();
         updateScoreInfo.setMatchId(matchTemp.getId());
         updateScoreInfo.setCompetitor1Score(score1);
         updateScoreInfo.setCompetitor2Score(score2);
         matchService.updateScore(updateScoreInfo);
-        
+
         Match match = matchRepo.findOne(matchTemp.getId());
         assertThat(match.getScore1(), equalTo(score1));
         assertThat(match.getScore2(), equalTo(score2));
