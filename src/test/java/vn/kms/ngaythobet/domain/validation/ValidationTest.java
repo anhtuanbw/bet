19eedf90584034a7afe35e81df056bf5e5b5e966
thangpham <thangpham@kms-technology.com>
2015-12-31 18:15:57 +0700

    Add new ValidationTest

--- src/test/java/vn/kms/ngaythobet/domain/validation/ValidationTest.java
+++ src/test/java/vn/kms/ngaythobet/domain/validation/ValidationTest.java
@@ -1,28 +1,45 @@
 // Copyright (c) 2015 KMS Technology, Inc.
 package vn.kms.ngaythobet.domain.validation;
 
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import vn.kms.ngaythobet.BaseTest;
-import vn.kms.ngaythobet.domain.core.User;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static vn.kms.ngaythobet.domain.util.Constants.WHITE_SPACE_REGEX;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 import javax.validation.constraints.Pattern;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.junit.After;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static vn.kms.ngaythobet.domain.util.Constants.WHITE_SPACE_REGEX;
+import vn.kms.ngaythobet.BaseTest;
+import vn.kms.ngaythobet.domain.core.User;
+import vn.kms.ngaythobet.domain.core.UserRepository;
+import vn.kms.ngaythobet.domain.tournament.Group;
+import vn.kms.ngaythobet.domain.tournament.GroupRepository;
+import vn.kms.ngaythobet.domain.tournament.Tournament;
+import vn.kms.ngaythobet.domain.tournament.TournamentRepository;
 
 public class ValidationTest extends BaseTest {
     @Autowired
     private Validator validator;
+
+    @Autowired
+    private TournamentRepository tournamentRepo;
+
+    @Autowired
+    private GroupRepository groupRepo;
+
+    @Autowired
+    private UserRepository userRepo;
 
     @Test
     public void testEntityExistValidation() {
@@ -101,13 +118,89 @@ public class ValidationTest extends BaseTest {
 
     @Test
     public void testListUniqueAndWhiteSpaceValidation() {
+        Tournament tournament = createTournament(true);
+
         List<Long> competitorIds = new ArrayList<>();
         competitorIds.add((long) 1);
         competitorIds.add((long) 1);
         competitorIds.add((long) 2);
-        TournamentData tournamentData = new TournamentData("   test white space     ", competitorIds);
+        TournamentData tournamentData = new TournamentData("   test white space     ", competitorIds,
+                tournament.getId());
         Set<ConstraintViolation<TournamentData>> violations = validator.validate(tournamentData);
         assertThat(violations.size(), equalTo(2));
+    }
+
+    @Test
+    public void testEntityValidatedValidation() {
+        Tournament tournament = createTournament(false);
+
+        List<Long> competitorIds = new ArrayList<>();
+        competitorIds.add((long) 1);
+        competitorIds.add((long) 2);
+
+        TournamentData tournamentData = new TournamentData("World Cup", competitorIds, tournament.getId());
+        Set<ConstraintViolation<TournamentData>> violations = validator.validate(tournamentData);
+        assertThat(violations.size(), equalTo(1));
+        ConstraintViolation<TournamentData> violation = violations.iterator().next();
+        assertThat(violation.getPropertyPath().toString(), equalTo("tournamentId"));
+        assertThat(violation.getMessage(), equalTo("Not activated"));
+    }
+
+    @Test
+    public void testModeratorAccessValidation() {
+        Tournament tournament = createTournament(true);
+
+        User tester = makeUser("Tester");
+        userRepo.save(tester);
+
+        List<Long> memberIds = new ArrayList<>();
+        memberIds.add(getDefaultUser().getId());
+        memberIds.add(tester.getId());
+
+        Group group = new Group();
+        group.setName("Launch 4");
+        group.setModerator(getDefaultUser());
+        group.setMembers(Collections.emptyList());
+        group.setTournament(tournament);
+        groupRepo.save(group);
+
+        mockLoginUser(getDefaultUser().getUsername());
+        MembersData membersData = new MembersData(group.getId(), memberIds);
+        Set<ConstraintViolation<MembersData>> violations = validator.validate(membersData);
+        violations = validator.validate(membersData);
+        assertThat(violations.size(), equalTo(0));
+
+        mockLoginUser(tester.getUsername());
+        violations = validator.validate(membersData);
+        assertThat(violations.size(), equalTo(1));
+        ConstraintViolation<MembersData> violation = violations.iterator().next();
+        assertThat(violation.getPropertyPath().toString(), equalTo("groupId"));
+        assertThat(violation.getMessage(), equalTo("Access deny, you are not moderator"));
+    }
+
+    @Test
+    public void testListEntityExistValidation() {
+        Tournament tournament = createTournament(true);
+
+        List<Long> memberIds = new ArrayList<>();
+        memberIds.add(getDefaultUser().getId());
+        memberIds.add((long) 99999);
+
+        Group group = new Group();
+        group.setName("Launch 4");
+        group.setModerator(getDefaultUser());
+        group.setMembers(Collections.emptyList());
+        group.setTournament(tournament);
+        groupRepo.save(group);
+
+        mockLoginUser(getDefaultUser().getUsername());
+        MembersData membersData = new MembersData(group.getId(), memberIds);
+        Set<ConstraintViolation<MembersData>> violations = validator.validate(membersData);
+        violations = validator.validate(membersData);
+        assertThat(violations.size(), equalTo(1));
+        ConstraintViolation<MembersData> violation = violations.iterator().next();
+        assertThat(violation.getPropertyPath().toString(), equalTo("memberIds"));
+        assertThat(violation.getMessage(), equalTo("Entity is not existed"));
     }
 
     static class TournamentData {
@@ -115,10 +208,13 @@ public class ValidationTest extends BaseTest {
         String tournamentName;
         @ListUnique
         List<Long> competitorIds;
+        @EntityActivated(type = Tournament.class)
+        Long tournamentId;
 
-        public TournamentData(String tournamentName, List<Long> competitorIds) {
+        public TournamentData(String tournamentName, List<Long> competitorIds, Long tournamentId) {
             this.tournamentName = tournamentName;
             this.competitorIds = competitorIds;
+            this.tournamentId = tournamentId;
         }
     }
 
@@ -147,4 +243,31 @@ public class ValidationTest extends BaseTest {
             this.manager = manager;
         }
     }
+
+    @ModeratorAccess
+    static class MembersData {
+        Long groupId;
+
+        @ListEntityExist(type = User.class)
+        List<Long> memberIds;
+
+        public MembersData(Long groupId, List<Long> memberIds) {
+            this.groupId = groupId;
+            this.memberIds = memberIds;
+        }
+    }
+
+    private Tournament createTournament(boolean isActivated) {
+        Tournament tournament = new Tournament();
+        tournament.setActivated(isActivated);
+        tournament.setName("World Cup");
+        return tournamentRepo.save(tournament);
+    }
+
+    @After
+    public void clearData() {
+        groupRepo.deleteAll();
+        tournamentRepo.deleteAll();
+    }
+
 }
