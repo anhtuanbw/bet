e112bbb3680b9c32b93c57d4b033f132a36e3049
anhtnguyen <anhtnguyen@kms-technology.com>
2016-01-13 13:01:32 +0700

    create getRoundNameByBettingMatchId

--- src/main/java/vn/kms/ngaythobet/domain/tournament/RoundRepository.java
+++ src/main/java/vn/kms/ngaythobet/domain/tournament/RoundRepository.java
@@ -4,10 +4,17 @@ package vn.kms.ngaythobet.domain.tournament;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RoundRepository extends JpaRepository<Round, Long> {
     Round findByName(String name);
     List<Round> findAllByOrderByCreatedAtDesc();
     List<Round> findByTournamentId(Long tournament_id);
     Round findByNameAndTournament(String name, Tournament tournament);
+    @Query("select r from BettingMatch bm "
+            + "inner join bm.match m "
+            + "inner join m.round r "
+            + "where bm.id = :bettingMatchId")
+    Round findByBettingMatchId(@Param("bettingMatchId") Long bettingMatchId);
 }
